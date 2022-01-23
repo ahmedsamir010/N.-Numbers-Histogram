35bb114cd2067020407ccf347ccbc787bfcf431a
ahmed samir sakr <89707420+ahmedsamir010@users.noreply.github.com>
2022-01-23 19:36:19 +0200

    Add files via upload

---
+++ N. Numbers Histogram.cpp
@@ -0,0 +1,30 @@
+﻿//Link Problem==>https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/M
+#include <iostream>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<algorithm>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pi 3.141592653589793238
+#define ahmed  ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL);
+#define no cout<<"NO"<<endl;
+#define yes cout<<"YES"<<endl;
+int counter[1000001] = {};
+int main()
+{
+    ahmed
+        char c; ll t, n;
+    cin >> c >> t;
+    while (t--) {
+        cin >> n;
+        while (n--) {
+            cout << c;
+        }
+        cout << endl;
+    }
+}
+//مهما الدنيا إتنيلت ضلمت لازم يبقى عندك زفت أمل و تعرف إن في نور في أخر أم النفق//
+
+        //اهو بنتنيل نستمر ف السعي رغم فقدان الزفت الشغف
